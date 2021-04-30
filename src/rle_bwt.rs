229210122c09223c883946e7a09f57b267f609cc
holtjma <jholt@hudsonalpha.org>
2021-04-30 14:12:49 -0500

    fm-index and constrain with initial tests passing

--- src/rle_bwt.rs
+++ src/rle_bwt.rs
@@ -13,17 +13,27 @@ pub struct RleBWT {
     total_counts: [u64; VC_LEN],
     start_index: [u64; VC_LEN],
     end_index: [u64; VC_LEN],
-    total_size: u64
+    fm_index: [Vec<u64>; VC_LEN],
+    ref_index: Vec<u64>,
+    total_size: u64,
+    bin_power: u8,
+    bin_size: u64
 }
 
 impl Default for RleBWT {
     fn default() -> Self {
+        let bin_power: u8 = 8;
+        let bin_size: u64 = 0x1 << bin_power;
         Self {
             bwt: vec![],
             total_counts: [0; VC_LEN],
             start_index: [0; VC_LEN],
             end_index: [0; VC_LEN],
-            total_size: 0
+            fm_index: Default::default(),
+            ref_index: Default::default(),
+            total_size: 0,
+            bin_power: bin_power,
+            bin_size: bin_size
         }
     }
 }
@@ -120,10 +130,88 @@ impl BWT for RleBWT {
     }
 
     unsafe fn constrain_range(&self, sym: u8, input_range: &BWTRange) -> BWTRange {
-        BWTRange {
-            l: 0,
-            h: 0
+        //first find the low value
+        let bin_id: usize = (input_range.l >> self.bin_power) as usize;
+        let mut compressed_index: usize = self.ref_index[bin_id] as usize;
+        let mut bwt_index: u64 = 0;
+        for x in 0..VC_LEN {
+            bwt_index += self.fm_index[x][bin_id];
         }
+        
+        let mut ret: BWTRange = Default::default();
+        ret.l = self.start_index[sym as usize]+self.fm_index[sym as usize][bin_id];
+        
+        let mut prev_char: u8 = 255;
+        let mut current_char: u8;
+        let mut prev_count: u64 = 0;
+        let mut power_multiple: u64 = 1;
+        
+        while bwt_index+prev_count < input_range.l {
+            current_char = self.bwt[compressed_index] & MASK;
+            if current_char == prev_char {
+                prev_count += (self.bwt[compressed_index] >> LETTER_BITS) as u64 * power_multiple;
+                power_multiple *= NUM_POWER as u64;
+            }
+            else {
+                if prev_char == sym {
+                    ret.l += prev_count;
+                }
+                
+                bwt_index += prev_count;
+                prev_count = (self.bwt[compressed_index] >> LETTER_BITS) as u64;
+                prev_char = current_char;
+                power_multiple = NUM_POWER as u64;
+            }
+            compressed_index += 1;
+        }
+
+        let temp_offset: u64 = ret.l;
+        if prev_char == sym {
+            ret.l += input_range.l - bwt_index as u64;
+        }
+
+        //now find the high value
+        let bin_id_h: usize = (input_range.h >> self.bin_power) as usize;
+        if bin_id == bin_id_h {
+            ret.h = temp_offset;
+        }
+        else {
+            compressed_index = self.ref_index[bin_id_h] as usize;
+            bwt_index = 0;
+            for x in 0..VC_LEN {
+                bwt_index += self.fm_index[x][bin_id_h];
+            }
+            
+            ret.h = self.start_index[sym as usize]+self.fm_index[sym as usize][bin_id_h];
+            
+            prev_char = 255;
+            prev_count = 0;
+            power_multiple = 1;
+        }
+        
+        while bwt_index+prev_count < input_range.h {
+            current_char = self.bwt[compressed_index] & MASK;
+            if current_char == prev_char {
+                prev_count += (self.bwt[compressed_index] >> LETTER_BITS) as u64 * power_multiple;
+                power_multiple *= NUM_POWER as u64;
+            }
+            else {
+                if prev_char == sym {
+                    ret.h += prev_count;
+                }
+                
+                bwt_index += prev_count;
+                prev_count = (self.bwt[compressed_index] >> LETTER_BITS) as u64;
+                prev_char = current_char;
+                power_multiple = NUM_POWER as u64;
+            }
+            compressed_index += 1;
+        }
+        
+        if prev_char == sym {
+            ret.h += input_range.h - bwt_index;
+        }
+        ret
     }
 }
 
@@ -135,7 +223,28 @@ impl RleBWT {
     /// let mut bwt: RleBWT = RleBWT::new();
     /// ```
     pub fn new() -> Self {
-        Default::default()
+        RleBWT::with_bin_power(8)
+    }
+
+    /// Allocation function for the BWT, look at `load_vector(...)` for initialization.
+    /// # Examples
+    /// ```rust
+    /// use msbwt::rle_bwt::RleBWT;
+    /// let mut bwt: RleBWT = RleBWT::new();
+    /// ```
+    pub fn with_bin_power(bin_power: u8) -> Self {
+        let bin_size: u64 = 0x1 << bin_power;
+        Self {
+            bwt: vec![],
+            total_counts: [0; VC_LEN],
+            start_index: [0; VC_LEN],
+            end_index: [0; VC_LEN],
+            fm_index: Default::default(),
+            ref_index: Default::default(),
+            total_size: 0,
+            bin_power: bin_power,
+            bin_size: bin_size
+        }
     }
 
     fn standard_init(&mut self) {
@@ -144,7 +253,7 @@ impl RleBWT {
         self.calculate_totals();
 
         //now we can construct the FM-index pieces in the binary storage format for rapid speed
-        //self.construct_fmindex(false);
+        self.construct_fmindex();
 
         /*
         //now do the fixed initialization
@@ -199,6 +308,89 @@ impl RleBWT {
         self.total_size = self.end_index[VC_LEN-1];
         info!("Loaded BWT with symbol counts: {:?}", self.total_counts);
     }
+
+    /// This will create the actual indexing structure. For RLE, it's just a sampled index
+    fn construct_fmindex(&mut self) {
+        //uint64_t samplingSize = (uint64_t)ceil(((float)this->totalSize+1)/this->binSize)+1;
+        //we have an extra +1 up there^; do we need that for some reason?
+        let index_length: usize = ((self.total_size as f64) / (self.bin_size as f64)).ceil() as usize + 1;
+        for subvec in self.fm_index.iter_mut() {
+            subvec.resize(index_length, 0);
+        }
+        self.ref_index.resize(index_length, 0);
+
+        //initial indices start at the start_index offsets
+        //let mut current_index: [u64; VC_LEN] = self.start_index.clone();
+        //initialize to all zeros
+        let mut current_index: [u64; VC_LEN] = [0; VC_LEN];
+
+        //decided to just set these all to usize since many are indices, seems relatively safe
+        let mut total_char_count: usize = 0;
+        let mut power_multiple: usize = 1;
+        let mut bin_end: usize = 0;
+        let mut bin_id: usize = 0;
+        let mut bwt_index: usize = 0;
+        let mut prev_start: usize = 0;
+        let mut prev_char: u8 = 0;
+        let mut current_char: u8;
+
+        //go through each run in the BWT and set FM-indices as we go
+        for (x, byte_value) in self.bwt.iter().enumerate() {
+            current_char = byte_value & MASK;
+            if current_char == prev_char {
+                total_char_count += (byte_value >> LETTER_BITS) as usize * power_multiple;
+                power_multiple *= NUM_POWER;
+            }
+            else {
+                //first save the current FM-index entry
+                //while bwt_index+total_char_count >= bin_end {
+                while bwt_index+total_char_count > bin_end {
+                    self.ref_index[bin_id] = prev_start as u64;
+                    for y in 0..VC_LEN {
+                        self.fm_index[y][bin_id] = current_index[y];
+                    }
+                    bin_end += self.bin_size as usize;
+                    bin_id += 1;
+                }
+                
+                //now add the previous
+                current_index[prev_char as usize] += total_char_count as u64;
+                bwt_index += total_char_count;
+                
+                prev_char = current_char;
+                prev_start = x;
+                total_char_count = (byte_value >> LETTER_BITS) as usize;
+                power_multiple = NUM_POWER;
+            }
+        }
+        
+        //fill out the remaining entries
+        //while bwt_index+total_char_count >= bin_end {
+        while bwt_index+total_char_count > bin_end {
+            self.ref_index[bin_id] = prev_start as u64;
+            for y in 0..VC_LEN {
+                self.fm_index[y][bin_id] = current_index[y];
+            }
+            bin_end += self.bin_size as usize;
+            bin_id += 1;
+        }
+        
+        //set the last entry
+        current_index[prev_char as usize] += total_char_count as u64;//forces countSoFar to hold the very end FM-index entry
+        self.ref_index[index_length-1] = self.bwt.len() as u64; //need to point to the index at the end
+        for y in 0..VC_LEN {
+            self.fm_index[y][index_length-1] = current_index[y];
+        }
+        /*
+        //calculate the total offset_sum
+        self.offset_sum = 0;
+        for y in 0..VC_LEN {
+            self.offset_sum += self.fm_index[y][0];
+        }
+        */
+        //next steps are to write tests for index construction
+        //we should make the index size a parameter for testing
+    }
 }
 
 #[cfg(test)]
@@ -223,20 +415,6 @@ mod tests {
             ret.push(r.as_bytes()[r.len()-1] as char);
         }
         ret
-    }
-
-    #[test]
-    fn test_constrain_range() {
-        let rle_bwt: RleBWT = Default::default();
-        let initial_range = BWTRange {
-            l: 0,
-            h: 0
-        };
-        let new_range = unsafe {
-            rle_bwt.constrain_range(0, &initial_range)
-        };
-
-        assert_eq!(new_range, BWTRange{l: 0, h:0});
     }
 
     fn write_strings_to_fqgz(data: Vec<&str>) -> NamedTempFile {
@@ -276,6 +454,140 @@ mod tests {
         for i in 0..6 {
             //make sure the total counts are correct
             assert_eq!(bwt.get_total_counts(i as u8), expected_totals[i]);
+        }
+    }
+
+    #[test]
+    fn test_fmindex() {
+        //strings - "CCGT\nACG\nN"
+        //build the BWT
+        let data: Vec<&str> = vec!["CCGT", "N", "ACG"];
+        
+        //stream and compress the BWT
+        //let bwt_stream = stream_bwt_from_fastqs(&fastq_filenames).unwrap();
+        let bwt_stream = naive_bwt(&data);
+        assert_eq!(bwt_stream, "GTN$$ACCC$G");
+        let compressed_bwt = convert_to_vec(bwt_stream.as_bytes());
+        //[G, T, N, 2$, A, 3C, $, G]
+        assert_eq!(compressed_bwt.len(), 8);
+        
+        //load it back in and verify counts
+        for bin_power in 1..5 {
+            let mut bwt = RleBWT::with_bin_power(bin_power);
+            bwt.load_vector(compressed_bwt.clone());
+
+            let expected_totals = vec![3, 1, 3, 2, 1, 1];
+            for i in 0..6 {
+                //make sure the total counts are correct
+                assert_eq!(bwt.get_total_counts(i as u8), expected_totals[i]);
+            }
+            
+            //make sure the chunk sizes are as expected
+            let expected_length: usize = (bwt_stream.len() as f64 / (0x1 << bin_power) as f64).ceil() as usize+1;
+            assert_eq!(bwt.ref_index.len(), expected_length);
+            for sym in 0..6 {
+                assert_eq!(bwt.fm_index[sym].len(), expected_length);
+            }
+
+            if bin_power == 1 {
+                //[G, T, N, 2$, A, 3C, $, G]
+                //every 2 bases should get an entry
+                let expected_ref: Vec<u64> = vec![0, 2, 3, 5, 5, 7, 8];
+                assert_eq!(bwt.ref_index, expected_ref);
+                let expected_fm_index: [Vec<u64>; VC_LEN] = [
+                    vec![0, 0, 0, 2, 2, 3, 3],
+                    vec![0, 0, 0, 1, 1, 1, 1],
+                    vec![0, 0, 0, 0, 0, 3, 3],
+                    vec![0, 1, 1, 1, 1, 1, 2],
+                    vec![0, 0, 1, 1, 1, 1, 1],
+                    vec![0, 1, 1, 1, 1, 1, 1]
+                ];
+                for i in 0..VC_LEN {
+                    assert_eq!(bwt.fm_index[i], expected_fm_index[i]);
+                }
+            } 
+            /*else if bin_power == 2 {
+                //every 4 bases should get an entry
+                let expected_ref: Vec<u64> = vec![0, 3, 5, 8];
+                assert_eq!(bwt.ref_index, expected_ref);
+                let expected_fm_index: [Vec<u64>; VC_LEN] = [
+                    vec![0, 0, 2, 3],
+                    vec![3, 3, 4, 4],
+                    vec![4, 4, 4, 7],
+                    vec![7, 8, 8, 9],
+                    vec![9, 10, 10, 10],
+                    vec![10, 11, 11, 11]
+                ];
+                for i in 0..VC_LEN {
+                    assert_eq!(bwt.fm_index[i], expected_fm_index[i]);
+                }
+            } else if bin_power == 3 {
+                //every 8 bases should get an entry
+                let expected_ref: Vec<u64> = vec![0, 5, 8];
+                assert_eq!(bwt.ref_index, expected_ref);
+                let expected_fm_index: [Vec<u64>; VC_LEN] = [
+                    vec![0, 2, 3],
+                    vec![3, 4, 4],
+                    vec![4, 4, 7],
+                    vec![7, 8, 9],
+                    vec![9, 10, 10],
+                    vec![10, 11, 11]
+                ];
+                for i in 0..VC_LEN {
+                    assert_eq!(bwt.fm_index[i], expected_fm_index[i]);
+                }
+            } else if bin_power == 4 {
+                //every 16 bases should get an entry
+                let expected_ref: Vec<u64> = vec![0, 8];
+                assert_eq!(bwt.ref_index, expected_ref);
+                let expected_fm_index: [Vec<u64>; VC_LEN] = [
+                    vec![0, 3],
+                    vec![3, 4],
+                    vec![4, 7],
+                    vec![7, 9],
+                    vec![9, 10],
+                    vec![10, 11]
+                ];
+                for i in 0..VC_LEN {
+                    assert_eq!(bwt.fm_index[i], expected_fm_index[i]);
+                }
+            }*/
+        }
+    }
+
+    #[test]
+    fn test_constrain_range() {
+        //strings - "CCGT\nACG\nN"
+        //build the BWT
+        let data: Vec<&str> = vec!["CCGT", "N", "ACG"];
+        
+        //stream and compress the BWT
+        //let bwt_stream = stream_bwt_from_fastqs(&fastq_filenames).unwrap();
+        let bwt_stream = naive_bwt(&data);
+        assert_eq!(bwt_stream, "GTN$$ACCC$G");
+        let compressed_bwt = convert_to_vec(bwt_stream.as_bytes());
+        //[G, T, N, 2$, A, 3C, $, G]
+        assert_eq!(compressed_bwt.len(), 8);
+        
+        //load it back in and verify counts
+        for bin_power in 1..5 {
+            let mut bwt = RleBWT::with_bin_power(bin_power);
+            bwt.load_vector(compressed_bwt.clone());
+
+            let initial_range = BWTRange {
+                l: 0,
+                h: bwt_stream.len() as u64
+            };
+            
+            //this is verifying that all single-symbol queries get the start/end range
+            for sym in 0..VC_LEN {
+                let new_range = unsafe {
+                    bwt.constrain_range(sym as u8, &initial_range)
+                };
+                assert_eq!(new_range, BWTRange{l: bwt.start_index[sym], h: bwt.end_index[sym]});
+            }
+
+            //now lets verify that we get all ascending symbols
         }
     }
 }
