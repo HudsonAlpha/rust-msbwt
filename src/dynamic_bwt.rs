691c9049c5f3cff076e8cf2929c35361dcea86d4
holtjma <jholt@hudsonalpha.org>
2021-09-10 10:44:09 -0500

    compiles but obviously fails

--- src/dynamic_bwt.rs
+++ src/dynamic_bwt.rs
@@ -1,4 +1,6 @@
 
+use crate::msbwt_core::*;
+
 use crate::run_block_av_flat::VC_LEN;
 use crate::rle_bplus_tree::RLEBPlusTree;
 use crate::string_util::convert_stoi;
@@ -33,6 +35,16 @@ impl Default for DynamicBWT {
 }
 
 impl DynamicBWT {
+    /// Allocation function for the BWT, look at `load_vector(...)` for initialization.
+    /// # Examples
+    /// ```rust
+    /// use msbwt2::dynamic_bwt::DynamicBWT;
+    /// let mut bwt = DynamicBWT::new();
+    /// ```
+    pub fn new() -> Self {
+        Default::default()
+    }
+
     #[inline]
     pub fn get_total_counts(&self) -> [usize; VC_LEN] {
         self.total_counts
@@ -134,26 +146,51 @@ impl DynamicBWT {
     }
 }
 
+impl BWT for DynamicBWT {
+    /// Initializes the BWT from a compressed BWT vector.
+    fn load_vector(&mut self, bwt: Vec<u8>) {
+        panic!("NO IMPL");
+    }
+
+    /// Initializes the BWT from the numpy file format for compressed BWTs
+    fn load_numpy_file(&mut self, filename: &str) -> std::io::Result<()> {
+        Err(
+            std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("NO IMPL")
+            )
+        )
+    }
+
+    /// Returns the total number of occurences of a given symbol
+    fn get_symbol_count(&self, symbol: u8) -> u64 {
+        panic!("NO IMPL");
+        0
+    }
+
+    /// This will return the total number of symbols contained by the BWT
+    fn get_total_size(&self) -> u64 {
+        panic!("NO IMPL");
+        0
+    }
+
+    /// Performs a range constraint on a BWT range. 
+    /// This implicitly represents prepending a character `sym` to a k-mer represented by `input_range` to create a new range representing a (k+1)-mer.
+    /// # Safety
+    /// This function is unsafe because there are no guarantees that the symbol or bounds will be checked by the implementing structure.
+    unsafe fn constrain_range(&self, sym: u8, input_range: &BWTRange) -> BWTRange {
+        panic!("NO IMPL");
+        Default::default()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::string_util::STRING_TO_INT;
-
-    fn naive_bwt(inputs: &Vec<&str>) -> Vec<u8> {
-        let mut rotations: Vec<String> = vec![];
-        for s in inputs.iter() {
-            let dollar_string = s.to_string()+&"$".to_string();
-            for l in 0..dollar_string.len() {
-                rotations.push(dollar_string[l..].to_string()+&dollar_string[..l]);
-            }
-        }
-        rotations.sort();
-        let mut ret: Vec<u8> = Vec::<u8>::with_capacity(rotations.len());
-        for r in rotations.iter() {
-            ret.push(STRING_TO_INT[r.as_bytes()[r.len()-1] as usize]);
-        }
-        ret
-    }
+    use crate::bwt_converter::*;
+    use crate::bwt_util::naive_bwt;
+    use crate::string_util;
+    use tempfile::{Builder, NamedTempFile};
 
     #[test]
     fn test_init() {
@@ -189,7 +226,7 @@ mod tests {
     #[test]
     fn test_multi_string_sorted() {
         let data: Vec<&str> = vec!["ACG", "N", "CCGT", "N", "ACG", "ACG", "CCGT", "N"];
-        let bwt = naive_bwt(&data);
+        let bwt = string_util::convert_stoi(&naive_bwt(&data));
         
         //insert the strings in the now sorted order
         let mut ubwt: DynamicBWT = Default::default();
@@ -203,8 +240,8 @@ mod tests {
     fn test_multi_length() {
         //getting bigger in order
         let data: Vec<&str> = vec!["A", "AA", "AAA", "AAAA", "AAAAA"];
-        let bwt = naive_bwt(&data);
-
+        let bwt = string_util::convert_stoi(&naive_bwt(&data));
+        
         let mut ubwt: DynamicBWT = Default::default();
         for s in data.iter() {
             ubwt.insert_string(s, true);
@@ -213,8 +250,8 @@ mod tests {
 
         //getting smaller in order
         let data: Vec<&str> = vec!["AAAAA", "AAAA", "AAA", "AA", "A"];
-        let bwt = naive_bwt(&data);
-
+        let bwt = string_util::convert_stoi(&naive_bwt(&data));
+        
         let mut ubwt: DynamicBWT = Default::default();
         for s in data.iter() {
             ubwt.insert_string(s, true);
@@ -241,13 +278,148 @@ mod tests {
         //println!("data: {:?}", data);
 
         //use this function to make a bwt the naive (i.e. slow) way
-        let naive = naive_bwt(&data);
-
+        let naive = string_util::convert_stoi(&naive_bwt(&data));
+        
         //insert the strings in the now sorted order
         let mut ubwt: DynamicBWT = Default::default();
         for s in data.iter() {
             ubwt.insert_string(s, true);
         }
         assert_eq!(ubwt.to_vec(), naive);
+    }
+
+    #[test]
+    fn test_load_rlebwt_from_npy() {
+        //strings - "CCGT\nACG\nN"
+        //build the BWT
+        let data: Vec<&str> = vec!["CCGT", "N", "ACG"];
+        
+        //stream and compress the BWT
+        //let bwt_stream = stream_bwt_from_fastqs(&fastq_filenames).unwrap();
+        let bwt_stream = naive_bwt(&data);
+        let compressed_bwt = convert_to_vec(bwt_stream.as_bytes());
+        
+        //save the output to a temporary numpy file
+        let bwt_file: NamedTempFile = Builder::new().prefix("temp_data_").suffix(".npy").tempfile().unwrap();
+        let filename: String = bwt_file.path().to_str().unwrap().to_string();
+        save_bwt_numpy(&compressed_bwt[..], &filename).unwrap();
+        
+        //load it back in and verify counts
+        let mut bwt = DynamicBWT::new();
+        bwt.load_numpy_file(&filename).unwrap();
+
+        let expected_totals = vec![3, 1, 3, 2, 1, 1];
+        for i in 0..6 {
+            //make sure the total counts are correct
+            assert_eq!(bwt.get_symbol_count(i as u8), expected_totals[i]);
+        }
+    }
+
+    #[test]
+    fn test_constrain_range() {
+        //strings - "CCGT\nACG\nN"
+        //build the BWT
+        let data: Vec<&str> = vec!["CCGT", "N", "ACG"];
+        
+        //stream and compress the BWT
+        //let bwt_stream = stream_bwt_from_fastqs(&fastq_filenames).unwrap();
+        let bwt_stream = naive_bwt(&data);
+        assert_eq!(bwt_stream, "GTN$$ACCC$G");
+        let bwt_int_form = string_util::convert_stoi(&bwt_stream);
+        let compressed_bwt = convert_to_vec(bwt_stream.as_bytes());
+        //[G, T, N, 2$, A, 3C, $, G]
+        assert_eq!(compressed_bwt.len(), 8);
+        
+        //load it back in and verify counts
+        let mut bwt: DynamicBWT = Default::default();
+        bwt.load_vector(compressed_bwt.clone());
+
+        let initial_range = BWTRange {
+            l: 0,
+            h: bwt_stream.len() as u64
+        };
+        
+        //this is verifying that all single-symbol queries get the start/end range
+        for sym in 0..VC_LEN {
+            let new_range = unsafe {
+                bwt.constrain_range(sym as u8, &initial_range)
+            };
+            assert_eq!(new_range, BWTRange{
+                l: bwt.offsets[sym] as u64, 
+                h: (bwt.offsets[sym]+bwt.total_counts[sym]) as u64
+            });
+        }
+
+        //now lets verify that we get all ascending symbols
+        for sym in 0..VC_LEN {
+            let mut sym_count = 0;
+            for ind in 0..(bwt_stream.len()+1) {
+                //test from 0 to the current index
+                let initial_range = BWTRange {
+                    l: 0,
+                    h: ind as u64
+                };
+
+                let new_range = unsafe {
+                    bwt.constrain_range(sym as u8, &initial_range)
+                };
+                assert_eq!(new_range, BWTRange {
+                    l: bwt.offsets[sym] as u64,
+                    h: (bwt.offsets[sym]+sym_count) as u64
+                });
+
+                //test from the current index to the high point
+                let initial_range = BWTRange {
+                    l: ind as u64,
+                    h: bwt_stream.len() as u64
+                };
+
+                let new_range = unsafe {
+                    bwt.constrain_range(sym as u8, &initial_range)
+                };
+                assert_eq!(new_range, BWTRange {
+                    l: (bwt.offsets[sym]+sym_count) as u64,
+                    h: (bwt.offsets[sym]+bwt.total_counts[sym]) as u64
+                });
+
+                //check if we need to adjust our expected values at all
+                if ind < bwt_stream.len() && bwt_int_form[ind] == sym as u8 {
+                    sym_count += 1;
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_count_kmer() {
+        //strings - "CCGT\nACG\nN"
+        //build the BWT
+        let data: Vec<&str> = vec!["CCGTACGTA", "GGTACAGTA", "ACGACGACG"];
+        
+        //stream and compress the BWT
+        //let bwt_stream = stream_bwt_from_fastqs(&fastq_filenames).unwrap();
+        let bwt_stream = naive_bwt(&data);
+        let compressed_bwt = convert_to_vec(bwt_stream.as_bytes());
+        
+        //load it back in and verify counts
+        let mut bwt: DynamicBWT = Default::default();
+        bwt.load_vector(compressed_bwt.clone());
+
+        //simple sanity checks, make sure our single-character symbols matches the total count
+        for c in 0..VC_LEN as u8 {
+            let test_seq: Vec<u8> = vec![c];
+            assert_eq!(bwt.get_symbol_count(c), bwt.count_kmer(&test_seq));
+        }
+        
+        //check that each string shows up once
+        for seq in data.iter() {
+            let test_seq = string_util::convert_stoi(seq);
+            assert_eq!(bwt.count_kmer(&test_seq), 1);
+        }
+
+        //now lets check some semi-arbitrary substrings
+        assert_eq!(bwt.count_kmer(&string_util::convert_stoi(&"ACG")), 4);
+        assert_eq!(bwt.count_kmer(&string_util::convert_stoi(&"CC")), 1);
+        assert_eq!(bwt.count_kmer(&string_util::convert_stoi(&"TAC")), 2);
     }
 }
